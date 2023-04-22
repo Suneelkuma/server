61cb16f26cbfb60cd1c481f75916bf3384c216f0
Suneelkuma <sunil24195@gmail.com>
2023-04-22 09:36:46 +0530

    added signin routes logout routes

--- routes/auth.js
+++ routes/auth.js
@@ -36,5 +36,61 @@ if(userExist){
         })
     }
 })
+// signin routes
 
+router.post('/signin',async(req,res)=>{
+    try {
+        const {name,email,phone,password,work}=req.body
+
+        if( !email|| !password){
+            return res.status(422).json({message:"Please fill all the data"})
+        }
+        const user=await User.findOne({email:email})
+        if(!user){
+            return res.status(422).json({message:"Invalid credentials"})
+        }
+//compare password here
+const userExist=await bcrypt.compare(password,user.password)
+
+// token generation using schema 
+let token =await user.generateAuthToken();
+//cookie storing
+
+res.cookie("jwttoken",token,{
+    expires:new Date(Date.now()+25892000000),
+    httpOnly:true
+})
+// res.cookie("jwtoken",token,{
+//     expires:new Date(Date.now()+25892000000),
+//     httpOnly:true
+//   });
+
+console.log(token);
+
+        if(!userExist){
+            return res.status(422).json({message:"Invalid credentials"})
+        }else{
+         
+            return res.status(200).json({message:"user login successfully"})
+        }
+        
+    } catch (error) {
+        res.status(400).json({
+            status:"Failed",
+            message:error.message
+        })
+    }
+})
+
+
+
+
+  // contactus
+router.get("/getdata", (req, res) => {
+    res.send(req.rootUser);
+  });
+  router.get("/logout", (req, res) => {
+    res.clearCookie('jwttoken',{path:'/'})
+    res.status(200).send("user logout");
+  });
 module.exports=router
